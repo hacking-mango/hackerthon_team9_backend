50a2a6617493ab98c0ef2b38720d2cef27da1667
춤추는망고 <ensia96@gmail.com>
2021-07-07 18:02:40 +0900

    [add] url pattern for user update view

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("signup/", views.user_view, name="sign-up"),
     path("login/", views.user_view, name="login"),
     path("info/", views.user_info_view, name="info"),
+    path("update/info/", views.user_view, name="user-update"),
 ]
