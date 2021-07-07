e840c6093d9bca45e00dc7022ecea1d9acf022a9
춤추는망고 <ensia96@gmail.com>
2021-07-07 19:00:57 +0900

    [add] url pattern for profile update view

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("signup/", views.user_view, name="sign-up"),
     path("login/", views.user_view, name="login"),
+    path("update/profile/", views.profile_update_view, name="profile-update"),
 ]
