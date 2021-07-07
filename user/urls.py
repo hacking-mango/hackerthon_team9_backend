d31bde7034404513a8a8f0f99cc47ac163dd5518
춤추는망고 <ensia96@gmail.com>
2021-07-07 20:50:53 +0900

    [fixed] typo in user_update_view url pattern

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("signup/", views.user_view, name="sign-up"),
     path("login/", views.user_view, name="login"),
     path("info/", views.user_info_view, name="info"),
-    path("update/info/", views.user_view, name="user-update"),
+    path("update/info/", views.user_update_view, name="user-update"),
 ]
