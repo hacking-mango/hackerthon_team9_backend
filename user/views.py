f67bac7740b11e0cc6ebfb2eabd0021b4c8c7b5d
춤추는망고 <ensia96@gmail.com>
2021-07-08 17:16:52 +0900

    [change] value of position_only variable

--- user/views.py
+++ user/views.py
@@ -91,7 +91,7 @@ def user_update_view(request):
 def profile_update_view(request):
 
     data = request.data
-    position_only = not data.pop("flag")
+    position_only = request.data.get("flag") == '0'
 
     def process(user, data, serializer, code, detail):
 
