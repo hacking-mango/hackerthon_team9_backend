def97a8e79f7087e3eeab037a43edb3b7e87c5b6
춤추는망고 <ensia96@gmail.com>
2021-07-08 15:52:10 +0900

    feat : 인증 함수 결과 값 변경 + 주석 추가

--- base/common/backends.py
+++ base/common/backends.py
@@ -8,7 +8,7 @@ from rest_framework import exceptions
 class Token(BaseAuthentication):
     def authenticate(self, request):
 
-        access_token = request.META.get("HTTP_TOKEN", None)
+        access_token = request.META.get("HTTP_TOKEN", None) # header: {'TOKEN' or 'token': token}
 
         if not access_token or access_token == "undefined":
             return None
@@ -19,10 +19,16 @@ class Token(BaseAuthentication):
             return None
 
         user = apps.get_model("user", "User")
-        user_index = decoded.get("id")
+        user_key = decoded.get("email")
 
         try:
-            user = user.objects.get(pk=user_index)
-            return user
+            user = user.objects.get(email=user_key)
+
+            user.is_authenticated = True
+
+            return (user, True)
+            # (user, auth) => (사용자, 인증 여부)
+            # https://www.django-rest-framework.org/api-guide/authentication/#custom-authentication
+
         except user.DoesNotExist:
             raise exceptions.AuthenticationFailed({"success": 0, "results": "authentication failed"})
