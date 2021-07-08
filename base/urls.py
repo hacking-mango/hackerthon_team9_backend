c29c91215bc28d3d7903634e307ec2a529120a2e
춤추는망고 <ensia96@gmail.com>
2021-07-08 23:31:53 +0900

    [create] chat app

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls"), name="user"),
+    path("chat/", include("chat.urls"), name="chat"),
 ]
