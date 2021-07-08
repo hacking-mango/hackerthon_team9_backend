14ef5a3982128a4a40d5d0a86429c3732b9af4e2
춤추는망고 <ensia96@gmail.com>
2021-07-08 23:36:03 +0900

    linting

--- base/settings/common.py
+++ base/settings/common.py
@@ -29,7 +29,7 @@ DJANGO_APPS = [
 
 THIRD_PARTY_APPS = ["rest_framework", "corsheaders", "storages"]
 
-LOCAL_APPS = ["user", 'chat']
+LOCAL_APPS = ["user", "chat"]
 
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
