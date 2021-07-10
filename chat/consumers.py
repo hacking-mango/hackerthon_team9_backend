8df0c093c9d79d6effd02d45584f217c01c3a34d
춤추는망고 <ensia96@gmail.com>
2021-07-10 16:14:43 +0900

    [change] room template & message group send function

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,25 +3,41 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+from user.models import User
+
+from .models import Message
+
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-
         async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
 
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name, {"type": "chat_message", "message": message})
-
     def chat_message(self, event):
         message = event["message"]
+        self.send(text_data=json.dumps(message))
 
-        self.send(text_data=json.dumps({"message": message}))
+    def receive(self, text_data):
+        data = json.loads(text_data)
+
+        author_user = User.objects.filter(email=data["from"])[0]
+
+        message = Message.objects.create(user=author_user, content=data["message"])
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": {
+                    "message": {
+                        "nickname": message.user.nickname,
+                        "content": message.content,
+                    },
+                },
+            },
+        )
