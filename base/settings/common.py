69935f18a90026670b223753afbe17e41c5d7e1c
wooseok1223 <tjdntjr123@gmail.com>
2021-07-10 17:02:45 +0900

    feat : aws s3 정적 파일 세팅 추가

--- base/settings/common.py
+++ base/settings/common.py
@@ -123,9 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
-
-
 # S3 영역
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
@@ -139,11 +136,20 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazo
 # 'CacheControl': 'max-age=86400',
 # }
 
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 AWS_S3_SECURE_URLS = False  # https 사용 여부
 AWS_QUERYSTRING_AUTH = False  # 인증 관련 쿼리 파라미터 허용 여부
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 # referenced web page1 : https://richone.tistory.com/7
 # referenced web page2 : https://ssungkang.tistory.com/entry/Django-AWS-S3를-이용한-이미지-업로드
 
