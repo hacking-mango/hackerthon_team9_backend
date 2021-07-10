b832444c85d4f3b3370a388b1b98dc2f8a05b859
춤추는망고 <ensia96@gmail.com>
2021-07-11 05:47:10 +0900

    [add] url pattern for chat related views

--- chat/urls.py
+++ chat/urls.py
@@ -4,10 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.index, name="index"),
-    url(r"^(?P<room_name>[^/]+)/$", views.room, name="room"),
+    # url(r"^$", views.index, name="index"),
+    # url(r"^(?P<room_name>[^/]+)/$", views.room, name="room"),
     path("matching/", views.matching_view, name="matching"),
     path("room/create/", views.create_room_view, name="create-room"),
     path("room/update/", views.update_room_view, name="update-room"),
     path("room/", views.get_room_view, name="get-room"),
+    path("room/leave", views.leave_room_view, name="leave-room"),
+    path("messages/", views.get_messages_view, name="get-messages"),
 ]
