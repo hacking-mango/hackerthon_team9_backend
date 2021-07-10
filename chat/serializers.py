617efffd1a195c5eeb555ec2ba78e0d07b7c0af5
춤추는망고 <ensia96@gmail.com>
2021-07-10 21:51:58 +0900

    [create] create room serializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,1 +1,37 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
 
+from .models import Room
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+
+    # 채팅방을 만드려면, 해시값 관련 규칙이 필요함
+    # def create(self, validated_data):
+    # room = Room.objects.create(
+    # room_hash=validated_data["room_hash"],
+    # activate=validated_data["activate"],
+    # nickname=validated_data["nickname"],
+    # max_planner=validated_data["max_planner"],
+    # max_designer=validated_data["max_designer"],
+    # max_frontend=validated_data["max_frontend"],
+    # max_backend=validated_data["max_backend"],
+    # max_aosdev=validated_data["max_aosdev"],
+    # max_iosdev=validated_data["max_iosdev"],
+    # )
+    # room.save()
+    # return room
+
+    class Meta:
+        model = Room
+        fields = [
+            "room_hash",
+            "activate",
+            "nickname",
+            "max_planner",
+            "max_designer",
+            "max_frontend",
+            "max_backend",
+            "max_aosdev",
+            "max_iosdev",
+        ]
