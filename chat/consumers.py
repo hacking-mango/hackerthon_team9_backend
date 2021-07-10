70c7b546e317f1c109cdeac3bd7d4740fa814c92
춤추는망고 <ensia96@gmail.com>
2021-07-11 05:45:28 +0900

    [add] database sync to async decorator to consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,8 @@
 import json
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
+from channels.db import database_sync_to_async
+from channels.generic.websocket import WebsocketConsumer
 
 from user.models import User
 
@@ -12,12 +13,16 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):  # 사용자가 접속했을 때 동작하는 함수
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-        print(self.room_group_name)
-        print(self.channel_layer)
-        # self.channel_layer.group_add(self.room_group_name, self.channel_name)
         async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
-        print(1313)
         self.accept()
+
+    @database_sync_to_async
+    def create_message(data):
+        author = User.objects.filter(email=data["from"])[0]
+
+        message = Message.objects.create(user=author, content=data["message"])
+
+        return message
 
     def disconnect(self, close_code):  # 사용자가 방에서 떠났을 때 동작하는 함수
         async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
@@ -27,13 +32,9 @@ class ChatConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps(message))
 
     def receive(self, text_data):  # 채팅방에 메시지가 전파되었을 때 동작하는 함수
-        print(text_data)
         data = json.loads(text_data)
-        print(data)
 
-        author_user = User.objects.filter(email=data["from"])[0]
-
-        message = Message.objects.create(user=author_user, content=data["message"])
+        message = create_message(data)
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -43,6 +44,8 @@ class ChatConsumer(WebsocketConsumer):
                     "message": {
                         "nickname": message.user.nickname,
                         "content": message.content,
+                        "created_at": message.created_at,
+                        "updated_at": message.updated_at,
                     },
                 },
             },
