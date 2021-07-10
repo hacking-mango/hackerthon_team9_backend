ca27f2518641f70c45a6b84d8bae9cc062582288
춤추는망고 <ensia96@gmail.com>
2021-07-10 16:18:47 +0900

    [add] comments to methods in chat consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,20 +9,20 @@ from .models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+    def connect(self):  # 사용자가 접속했을 때 동작하는 함수
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
         async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
 
-    def disconnect(self, close_code):
+    def disconnect(self, close_code):  # 사용자가 방에서 떠났을 때 동작하는 함수
         async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
 
-    def chat_message(self, event):
+    def chat_message(self, event): # 실제로 메시지를 전송하는 함수
         message = event["message"]
         self.send(text_data=json.dumps(message))
 
-    def receive(self, text_data):
+    def receive(self, text_data): # 채팅방에 메시지가 전파되었을 때 동작하는 함수
         data = json.loads(text_data)
 
         author_user = User.objects.filter(email=data["from"])[0]
