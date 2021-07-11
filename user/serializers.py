46f2e678a3206645412ba99d6e79de06b785bd8e
wooseok1223 <tjdntjr123@gmail.com>
2021-07-11 11:54:39 +0900

    fix : 회원가입시 이미지, 사진 있을때만 저장

--- user/serializers.py
+++ user/serializers.py
@@ -11,13 +11,16 @@ class SignupSerializer(serializers.ModelSerializer):
 
         user = models.User.objects.create(
             email=validated_data["email"],
-            password=validated_data["password"],
             nickname=validated_data["nickname"],
             age=validated_data["age"],
             phone=validated_data["phone"],
-            position=validated_data["position"],
-            profile_image=validated_data["profile_image"],
         )
+        if validated_data["position"]:
+            user.position = validated_data["position"]
+
+        if validated_data["profile_image"]:
+            user.profile_image = validated_data["profile_image"]
+
         password = make_password(validated_data["password"])
         user.password = password
         user.save()
